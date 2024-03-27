2aa17744ae4ffeaa69273dfe4dd3fa04bc400c92
Anthony <145401343+AnthonyP365@users.noreply.github.com>
2024-03-27 14:18:38 -0400

    get test

--- src/main/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedList.java
+++ src/main/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedList.java
@@ -3,5 +3,109 @@ package com.zipcodewilmington.singlylinkedlist;
 /**
  * Created by leon on 1/10/18.
  */
-public class SinglyLinkedList {
+public class SinglyLinkedList<T> {
+//  Inner class representing individual nodes in the linked list
+    private static class Node<T> {
+//      Stores the value of the node
+        T data;
+//      A reference to the next node in the linked list
+        Node<T> next;
+
+        Node(T data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+//  Represents the starting point of the linked list. Stores a reference to the first node
+    private Node<T> head;
+
+//  Initializes the head pointer to null, indicating that the linked list starts as empty
+    public SinglyLinkedList() {
+        this.head = null;
+    }
+
+    public void add(T data) {
+        Node<T> node = new Node<>(data); // Create a new node with the given data
+        if (head == null) {
+            head = node; // If the list is empty, make the new node the head of the list
+        } else {
+            Node<T> current = head;
+            while (current.next != null) {
+                current = current.next; // Move the next node until the last node is reached
+            }
+            current.next = node; // Set the next pointer of the last node to the new node
+        }
+    }
+
+    public void remove(int index) {
+        Node<T> current = head;
+        Node<T> previous = null;
+
+        if (index == 0) {
+            head = current.next; // If index is 0, moves the head to next link
+        } else if (index > 0) {
+            int currentIndex = 0;
+            while (current != null && currentIndex < index) { // iterates through the list until it lands on the index
+                previous = current;
+                current = current.next;
+                currentIndex++;
+            }
+            previous.next = current.next; // Skips (deletes) the data at the index
+        }
+    }
+
+    public Boolean contains(T data) {
+        boolean result = false;
+        Node<T> current = head;
+
+        while (current != null) {
+            if (current.data.equals(data)) {
+                result = true;
+            }
+            current = current.next;
+        }
+        return result;
+    }
+
+    public Integer find(T data) {
+        int index = 0;
+        Node<T> current = head;
+
+        while (current != null) {
+            if (current.data.equals(data)) {
+                return index;
+            }
+            current = current.next;
+            index++;
+        }
+        return -1;
+    }
+
+    public Integer size() {
+        int size = 1;
+        Node<T> current = head;
+
+        while (current != null) {
+            if (current.next == null) {
+                return size;
+            } else {
+                size++;
+                current = current.next;
+            }
+        }
+        return size;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        Node<T> current = head;
+
+        while (current != null) {
+            sb.append(current.data).append(" ");
+            current = current.next;
+        }
+        return sb.toString().trim();
+    }
 }
--- src/test/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedListTest.java
+++ src/test/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedListTest.java
@@ -1,7 +1,110 @@
 package com.zipcodewilmington.singlylinkedlist;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 /**
  * Created by leon on 1/10/18.
  */
 public class SinglyLinkedListTest {
+
+    @Test
+    public void addTest() {
+        SinglyLinkedList<Integer> list = new SinglyLinkedList<>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+
+        String expectedList = "1 2 3";
+        String actualList = list.toString();
+
+        Assert.assertEquals(expectedList, actualList);
+    }
+
+    @Test
+    public void removeTest() {
+        SinglyLinkedList<Integer> list = new SinglyLinkedList<>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+
+        String expectedList = "1 2";
+        list.remove(2);
+
+        Assert.assertEquals(expectedList, list.toString());
+    }
+
+    @Test
+    public void containsTest1() {
+        SinglyLinkedList<Integer> list = new SinglyLinkedList<>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+
+        Assert.assertTrue(list.contains(2));
+    }
+
+    @Test
+    public void containsTest2() {
+        SinglyLinkedList<Integer> list = new SinglyLinkedList<>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+
+        Assert.assertFalse(list.contains(4));
+    }
+
+    @Test
+    public void findTest1() {
+        SinglyLinkedList<Integer> list = new SinglyLinkedList<>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+
+        Integer expectedIndex = 1;
+        Integer actualIndex = list.find(2);
+
+        Assert.assertEquals(expectedIndex, actualIndex);
+    }
+
+    @Test
+    public void findTest2() {
+        SinglyLinkedList<Integer> list = new SinglyLinkedList<>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+
+        Integer expectedIndex = -1;
+        Integer actualIndex = list.find(4);
+
+        Assert.assertEquals(expectedIndex, actualIndex);
+    }
+
+    @Test
+    public void sizeTest() {
+        SinglyLinkedList<Integer> list = new SinglyLinkedList<>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+
+        Integer expectedSize = 3;
+        Integer actualSize = list.size();
+
+        Assert.assertEquals(expectedSize, actualSize);
+    }
+
+    @Test
+    public void getTest() {}
+
+    @Test
+    public void copyTest() {}
+
+    @Test
+    public void sortTest() {}
+
+    @Test
+    public void reverseTest() {}
+
+    @Test
+    public void sliceTest() {}
 }
